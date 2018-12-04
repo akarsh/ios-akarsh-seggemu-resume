68514f566c79bc94aca43bcda63877707622f325
akarsh <sakarshkumar@gmail.com>
2018-12-04 01:02:11 +0100

    Added Localisation for English and Deutsch
    - Localisation strings file is added.
    - Updated MainTableViewController to send language code based on the selection.
    - Updated ResumeSchemaLayout to display based on the language code received from MainTableViewController.

---
+++ akarsh seggemu resume/UILabel+Localization.swift
@@ -0,0 +1,13 @@
+//
+//  extendUILabelForLocalization.swift
+//  akarsh seggemu resume
+//
+//  Created by Akarsh Seggemu on 03/12/2018.
+//  Copyright © 2018 Akarsh Seggemu. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    
+}
