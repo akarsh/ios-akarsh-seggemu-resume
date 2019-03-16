be3254520620b87f9e781eb22a035c8c199ce87c
akarsh <sakarshkumar@gmail.com>
2019-03-16 23:41:03 +0100

    Base class is added to the UITests - ASR is used as short form of the project name - akarsh seggemu resume - ASR_BaseUITests is the name of the file

---
+++ akarsh seggemu resumeUITests/ASR_BaseUITests.swift
@@ -0,0 +1,10 @@
+//
+//  ASR_BaseUITests.swift
+//  akarsh seggemu resumeUITests
+//
+//  Created by Akarsh Seggemu on 16.03.19.
+//  Copyright © 2019 Akarsh Seggemu. All rights reserved.
+//
+
+import Foundation
+
