caf7d02e56bb78984c6ff41e3fbe4e00dcff956d
akarsh <sakarshkumar@gmail.com>
2019-03-17 01:46:54 +0100

    Created four constants to store the values of table cell content and view names - UITests class now extends BaseUITests class   - The setup and tear down methods are moved to base class - two navigation test methods are refactored to use for loop to get,   -  the names of the view from the array -  resumeNavigationBarsContent   - the contents of the cell from the array - englishResumeCellContent and deutschLebenslaufCellContent

--- akarsh seggemu resumeUITests/akarsh_seggemu_resumeUITests.swift
+++ akarsh seggemu resumeUITests/akarsh_seggemu_resumeUITests.swift
@@ -8,86 +8,72 @@
 
 import XCTest
 
-class akarsh_seggemu_resumeUITests: XCTestCase {
-    var app: XCUIApplication!
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        app = XCUIApplication()
-        app.launch()
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+class akarsh_seggemu_resumeUITests: ASR_BaseUITests {
+    let resumeMainCellContent = [
+        "English resume",
+        "Deutsch Lebenslauf"
+    ]
+    let resumeNavigationBarsContent = [
+        "akarsh_seggemu_resume.ContactLayoutView",
+        "akarsh_seggemu_resume.InfoLayoutView",
+        "akarsh_seggemu_resume.SummaryLayoutView",
+        "akarsh_seggemu_resume.ProfilesLayoutTableView",
+        "akarsh_seggemu_resume.SkillsLayoutTableView",
+        "akarsh_seggemu_resume.LanguagesLayoutTableView",
+        "akarsh_seggemu_resume.EducationLayoutTableView",
+        "akarsh_seggemu_resume.ExperienceLayoutTableView",
+        "akarsh_seggemu_resume.VolunteerLayoutTableView",
+        "akarsh_seggemu_resume.AwardsLayoutTableView",
+        "akarsh_seggemu_resume.PublicationsLayoutTableView",
+        "akarsh_seggemu_resume.InterestsLayoutTableView",
+        "akarsh_seggemu_resume.ReferencesLayoutTableView"
+    ]
+    let englishResumeCellContent = [
+        "Contact",
+        "Info",
+        "Summary",
+        "Profiles",
+        "Skills",
+        "Languages",
+        "Education",
+        "Experience",
+        "Volunteer",
+        "Awards",
+        "Publications",
+        "Interests",
+        "References"
+    ]
+    let deutschLebenslaufCellContent = [
+        "Kontakt",
+        "Info",
+        "Zusammenfassung",
+        "Profil",
+        "Fähigkeiten",
+        "Sprachen",
+        "Ausbildung",
+        "Berufserfahrung",
+        "Gemeinnützig",
+        "Auszeichnung",
+        "Publikation",
+        "Interessen",
+        "Referenzen"
+    ]
     func testNavigationintoIntoEnglishResume() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
         let app = XCUIApplication()
         let tablesQuery = app.tables
-        tablesQuery.staticTexts["English resume"].tap()
-        tablesQuery.staticTexts["Contact"].tap()
-        app.navigationBars["akarsh_seggemu_resume.ContactLayoutView"].buttons["Back"].tap()
-        tablesQuery.staticTexts["Info"].tap()
-        app.navigationBars["akarsh_seggemu_resume.InfoLayoutView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Summary"]/*[[".cells.staticTexts[\"Summary\"]",".staticTexts[\"Summary\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.SummaryLayoutView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Profiles"]/*[[".cells.staticTexts[\"Profiles\"]",".staticTexts[\"Profiles\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.ProfilesLayoutTaableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Skills"]/*[[".cells.staticTexts[\"Skills\"]",".staticTexts[\"Skills\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.SkillsLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Languages"]/*[[".cells.staticTexts[\"Languages\"]",".staticTexts[\"Languages\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.LanguagesLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Education"]/*[[".cells.staticTexts[\"Education\"]",".staticTexts[\"Education\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.EducationLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Experience"]/*[[".cells.staticTexts[\"Experience\"]",".staticTexts[\"Experience\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.ExperienceLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Volunteer"]/*[[".cells.staticTexts[\"Volunteer\"]",".staticTexts[\"Volunteer\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.VolunteerLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Awards"]/*[[".cells.staticTexts[\"Awards\"]",".staticTexts[\"Awards\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.AwardsLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Publications"]/*[[".cells.staticTexts[\"Publications\"]",".staticTexts[\"Publications\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.PublicationsLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Interests"]/*[[".cells.staticTexts[\"Interests\"]",".staticTexts[\"Interests\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.InterestsLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["References"]/*[[".cells.staticTexts[\"References\"]",".staticTexts[\"References\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.ReferencesLayoutTableView"].buttons["Back"].tap()
+        tablesQuery.staticTexts[resumeMainCellContent[0]].tap()
+        for (index, element) in englishResumeCellContent.enumerated() {
+            tablesQuery.staticTexts[element].tap()
+            app.navigationBars[resumeNavigationBarsContent[index]].buttons["Back"].tap()
+        }
     }
-
     func testNavigationintoIntoDeutschLebenslauf() {
         let app = XCUIApplication()
         let tablesQuery = app.tables
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Deutsch Lebenslauf"]/*[[".cells.staticTexts[\"Deutsch Lebenslauf\"]",".staticTexts[\"Deutsch Lebenslauf\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Kontakt"]/*[[".cells.staticTexts[\"Kontakt\"]",".staticTexts[\"Kontakt\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.ContactLayoutView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Info"]/*[[".cells.staticTexts[\"Info\"]",".staticTexts[\"Info\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.InfoLayoutView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Zusammenfassung"]/*[[".cells.staticTexts[\"Zusammenfassung\"]",".staticTexts[\"Zusammenfassung\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.SummaryLayoutView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Profil"]/*[[".cells.staticTexts[\"Profil\"]",".staticTexts[\"Profil\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.ProfilesLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Fähigkeiten"]/*[[".cells.staticTexts[\"Fähigkeiten\"]",".staticTexts[\"Fähigkeiten\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.SkillsLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Sprachen"]/*[[".cells.staticTexts[\"Sprachen\"]",".staticTexts[\"Sprachen\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.LanguagesLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Ausbildung"]/*[[".cells.staticTexts[\"Ausbildung\"]",".staticTexts[\"Ausbildung\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.EducationLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Berufserfahrung"]/*[[".cells.staticTexts[\"Berufserfahrung\"]",".staticTexts[\"Berufserfahrung\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.ExperienceLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Gemeinnützig"]/*[[".cells.staticTexts[\"Gemeinnützig\"]",".staticTexts[\"Gemeinnützig\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.VolunteerLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Auszeichnung"]/*[[".cells.staticTexts[\"Auszeichnung\"]",".staticTexts[\"Auszeichnung\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.AwardsLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Publikation"]/*[[".cells.staticTexts[\"Publikation\"]",".staticTexts[\"Publikation\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.PublicationsLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Interessen"]/*[[".cells.staticTexts[\"Interessen\"]",".staticTexts[\"Interessen\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.InterestsLayoutTableView"].buttons["Back"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Referenzen"]/*[[".cells.staticTexts[\"Referenzen\"]",".staticTexts[\"Referenzen\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["akarsh_seggemu_resume.ReferencesLayoutTableView"].buttons["Back"].tap()
+        tablesQuery.staticTexts[resumeMainCellContent[1]].tap()
+        for (index, element) in deutschLebenslaufCellContent.enumerated() {
+            tablesQuery.staticTexts[element].tap()
+            app.navigationBars[resumeNavigationBarsContent[index]].buttons["Back"].tap()
+        }
     }
 }
