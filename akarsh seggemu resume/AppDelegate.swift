8068c0a6126dfc58402c61e4a8bb858016cb8c4f
Akarsh Seggemu <sakarshkumar@gmail.com>
2021-03-03 21:39:30 +0100

    feat(delegate): update AppDelegate to support UISceneSession lifecycle

--- akarsh seggemu resume/AppDelegate.swift
+++ akarsh seggemu resume/AppDelegate.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import SwiftUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
