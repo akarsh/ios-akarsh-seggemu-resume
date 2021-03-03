0ae259862192862879859e96c41cbbd91024b713
Akarsh Seggemu <sakarshkumar@gmail.com>
2021-03-03 23:35:04 +0100

    feat(view): add resumeSchema view

---
+++ akarsh seggemu resume/View/ResumeSchemaView.swift
@@ -0,0 +1,21 @@
+//
+//  ResumeSchemaView.swift
+//  akarsh seggemu resume
+//
+//  Created by Akarsh Seggemu on 03.03.21.
+//  Copyright © 2021 Akarsh Seggemu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ResumeSchemaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResumeSchemaView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResumeSchemaView()
+    }
+}
