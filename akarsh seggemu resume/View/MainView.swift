9bcc7ee10236781dd8e01e82ec6ef3b743a13b28
Akarsh Seggemu <sakarshkumar@gmail.com>
2021-03-03 21:41:48 +0100

    feat(delegate-and-view): add SceneDelegate and MainView files to the project
    - Update Info.plist to use SceneDelegate
      - Remove UIStoryboard - Main

---
+++ akarsh seggemu resume/View/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  akarsh seggemu resume
+//
+//  Created by Akarsh Seggemu on 03.03.21.
+//  Copyright © 2021 Akarsh Seggemu. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
